200c103750f5a001188dafd9684373ff6fe5f131
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2023-11-30 19:59:22 +0100

    -a

--- js/script.js
+++ js/script.js
@@ -1,48 +1,44 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const countriesListContainer = document.getElementById('countries-list');
     const countryInfoContainer = document.getElementById('country-info');
-    const closeBtn = document.getElementById('close-btn');
-    const flag = document.getElementById('flag');
+        const flag = document.getElementById('flag');
     const capital = document.getElementById('capital');
     const population = document.getElementById('population');
     const road = document.getElementById('road');
     const overlay = document.getElementById('overlay');
+    const img = document.getElementById('img');
 
-    // Obtener datos de la API
+    //api
     const countries = await fetchCountries();
-    // Ordenar países alfabéticamente (ignorando mayúsculas y minúsculas)
+    
+    // Ordenar países 
     countries.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
 
-    // Renderizar lista de países
+//trar paises
     countries.forEach(country => {
         const flagImg = document.createElement('img');
         flagImg.src = country.flags[0];
         flagImg.alt = `${country.name} Flag`;
         flagImg.addEventListener('click', () => showCountryInfo(country));
         countriesListContainer.appendChild(flagImg);
+        countriesListContainer.appendChild(flag);
+      
     });
 
-    // Mostrar información detallada del país
+    //  información 
     function showCountryInfo(country) {
         flag.src = country.flags[0];
-        capital.textContent = `Capital: ${country.capital[0] || 'N/A'}`;
-        population.textContent = `Población: ${country.population || 'N/A'}`;
-        road.textContent = `Lado de la carretera: ${country.trafficRules ? country.trafficRules.driving_side || 'N/A' : 'N/A'}`;
-
+        capital.textContent = `Capital: ${country.capital[0] }`;
+        population.textContent = `Población: ${country.population || z}`;
         countryInfoContainer.classList.remove('hidden');
         overlay.classList.remove('hidden');
     }
 
-    // Cerrar la ventana flotante
-    function closeCountryInfo() {
-        countryInfoContainer.classList.add('hidden');
-        overlay.classList.add('hidden');
-    }
+    
+    closeBtn.addEventListener('click', closeBtn);
+    overlay.addEventListener('click', closeBtn);
 
-    closeBtn.addEventListener('click', closeCountryInfo);
-    overlay.addEventListener('click', closeCountryInfo);
-
-    // Función para obtener datos de la API
+    //obtuener api
     async function fetchCountries() {
         const response = await fetch('https://restcountries.com/v2/all');
         const data = await response.json();
